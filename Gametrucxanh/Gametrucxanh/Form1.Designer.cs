6cfef455b98a897e6cf3565ac651b667b64d02eb
QuangThoai <2001150046@ittc.edu.vn>
2018-08-26 19:20:21 +0700

    BTAP
    game trúc xanh

---
+++ Gametrucxanh/Gametrucxanh/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace Gametrucxanh
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_sohinh = new System.Windows.Forms.TextBox();
+            this.btn_start = new System.Windows.Forms.Button();
+            this.gbox_trucxanh = new System.Windows.Forms.GroupBox();
+            this.gbcontrol = new System.Windows.Forms.GroupBox();
+            this.gbtt = new System.Windows.Forms.GroupBox();
+            this.txt_design = new System.Windows.Forms.RichTextBox();
+            this.gbdark = new System.Windows.Forms.GroupBox();
+            this.tgian = new System.Windows.Forms.Label();
+            this.gbcontrol.SuspendLayout();
+            this.gbtt.SuspendLayout();
+            this.gbdark.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txt_sohinh
+            // 
+            this.txt_sohinh.Location = new System.Drawing.Point(11, 22);
+            this.txt_sohinh.Name = "txt_sohinh";
+            this.txt_sohinh.Size = new System.Drawing.Size(100, 20);
+            this.txt_sohinh.TabIndex = 0;
+            // 
+            // btn_start
+            // 
+            this.btn_start.Location = new System.Drawing.Point(22, 48);
+            this.btn_start.Name = "btn_start";
+            this.btn_start.Size = new System.Drawing.Size(75, 23);
+            this.btn_start.TabIndex = 1;
+            this.btn_start.Text = "Start";
+            this.btn_start.UseVisualStyleBackColor = true;
+            this.btn_start.Click += new System.EventHandler(this.btn_start_Click);
+            // 
+            // gbox_trucxanh
+            // 
+            this.gbox_trucxanh.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.gbox_trucxanh.Location = new System.Drawing.Point(26, 106);
+            this.gbox_trucxanh.Name = "gbox_trucxanh";
+            this.gbox_trucxanh.Size = new System.Drawing.Size(983, 266);
+            this.gbox_trucxanh.TabIndex = 2;
+            this.gbox_trucxanh.TabStop = false;
+            this.gbox_trucxanh.Text = "Trúc xanh";
+            // 
+            // gbcontrol
+            // 
+            this.gbcontrol.Controls.Add(this.txt_sohinh);
+            this.gbcontrol.Controls.Add(this.btn_start);
+            this.gbcontrol.Location = new System.Drawing.Point(12, 12);
+            this.gbcontrol.Name = "gbcontrol";
+            this.gbcontrol.Size = new System.Drawing.Size(232, 82);
+            this.gbcontrol.TabIndex = 3;
+            this.gbcontrol.TabStop = false;
+            this.gbcontrol.Text = "Controls Game";
+            // 
+            // gbtt
+            // 
+            this.gbtt.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.gbtt.Controls.Add(this.txt_design);
+            this.gbtt.Location = new System.Drawing.Point(810, 2);
+            this.gbtt.Name = "gbtt";
+            this.gbtt.Size = new System.Drawing.Size(200, 110);
+            this.gbtt.TabIndex = 4;
+            this.gbtt.TabStop = false;
+            this.gbtt.Text = "Thông tin";
+            // 
+            // txt_design
+            // 
+            this.txt_design.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txt_design.Location = new System.Drawing.Point(6, 19);
+            this.txt_design.Name = "txt_design";
+            this.txt_design.Size = new System.Drawing.Size(188, 81);
+            this.txt_design.TabIndex = 0;
+            this.txt_design.Text = "";
+            // 
+            // gbdark
+            // 
+            this.gbdark.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.gbdark.Controls.Add(this.tgian);
+            this.gbdark.Location = new System.Drawing.Point(250, 12);
+            this.gbdark.Name = "gbdark";
+            this.gbdark.Size = new System.Drawing.Size(560, 100);
+            this.gbdark.TabIndex = 5;
+            this.gbdark.TabStop = false;
+            this.gbdark.Text = "DARKMOON";
+            // 
+            // tgian
+            // 
+            this.tgian.AutoSize = true;
+            this.tgian.Location = new System.Drawing.Point(62, 39);
+            this.tgian.Name = "tgian";
+            this.tgian.Size = new System.Drawing.Size(49, 13);
+            this.tgian.TabIndex = 0;
+            this.tgian.Text = "0000000";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1022, 385);
+            this.Controls.Add(this.gbdark);
+            this.Controls.Add(this.gbtt);
+            this.Controls.Add(this.gbcontrol);
+            this.Controls.Add(this.gbox_trucxanh);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.gbcontrol.ResumeLayout(false);
+            this.gbcontrol.PerformLayout();
+            this.gbtt.ResumeLayout(false);
+            this.gbdark.ResumeLayout(false);
+            this.gbdark.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_sohinh;
+        private System.Windows.Forms.Button btn_start;
+        private System.Windows.Forms.GroupBox gbox_trucxanh;
+        private System.Windows.Forms.GroupBox gbcontrol;
+        private System.Windows.Forms.GroupBox gbtt;
+        private System.Windows.Forms.RichTextBox txt_design;
+        private System.Windows.Forms.GroupBox gbdark;
+        private System.Windows.Forms.Label tgian;
+    }
+}
+
---
+++ Gametrucxanh/Gametrucxanh/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+
+namespace Gametrucxanh
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Random r = new Random();
+        int n;
+        private void btn_start_Click(object sender, EventArgs e)
+        {
+            
+            n= Int32.Parse( txt_sohinh.Text.ToString());
+            ArrayList hinh = new ArrayList();
+            int k = 0, j = 0;
+            for (int i = 0; i < n*2; i++)
+            {
+                Button btnhinh = new Button();
+                btnhinh.Name = "btn" + i.ToString();
+                btnhinh.Width = 150;
+                btnhinh.Height = 150;
+                btnhinh.Top = 10 + j * 150;
+                btnhinh.Left = 10 + k * 150;
+                btnhinh.Image = Image.FromFile("Anh/anhnen.png");
+                btnhinh.Click += btnhinh_Click;
+                if (i < n)
+                {
+                    int stt = r.Next(0,n);
+                    btnhinh.Tag = stt;
+                    hinh.Add(stt);
+                }
+                else
+                {
+                    int stt = r.Next(0, hinh.Count - 1); // Lấy ngẫu nhiên 1 index trong list.
+                    btnhinh.Tag = hinh[stt];//random phần tử có index x trong list.
+                    hinh.RemoveAt(stt);
+                }
+                gbox_trucxanh.Controls.Add(btnhinh);
+                k++;
+                if (k == 6)
+                {
+                    k = 0; j++;
+                }
+            }
+        }
+        int solanbam = 0;
+        string a, b, nut1, nut2;
+        void btnhinh_Click(object sender, EventArgs e)
+        {
+            solanbam++;
+            int id = (int)((Button)sender).Tag;
+            //MessageBox.Show("Nút số \t" + id.ToString() +((Button)sender).Name.ToString() );
+            ((Button)sender).Image = Image.FromFile(id.ToString() + ".jpg");
+            ((Button)sender).Enabled = false;
+            if (solanbam % 2 != 0)
+            {
+                a = ((Button)sender).Name.ToString();
+                nut1 = ((Button)sender).Tag.ToString();
+            }
+            else
+            {
+                b = ((Button)sender).Name.ToString();
+                nut2 = ((Button)sender).Tag.ToString();
+            }
+            if (solanbam % 2 == 0)
+            {
+                ktrahinh(a,b,nut1,nut2);
+            }
+        }
+        void ktrahinh(string name1, string name2,string tag1,string tag2)
+        {
+            if (solanbam %2==0)
+            {
+                foreach (Button btn in gbox_trucxanh.Controls)
+                {
+                    if (btn.Enabled == false)
+                    {
+                        System.Threading.Thread.Sleep(500);
+                        if (String.Compare(b, a) != 0 && String.Compare(tag1,tag2) == 0)
+                        {
+                           
+                            btn.Visible = false;
+                        }
+                        else
+                        {
+                             btn.Image = Image.FromFile("Anh/anhnen.png");
+                             btn.Enabled = true;
+                           
+                        }
+                    }
+                }
+                
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txt_design.Text = "Trò chơi được tạo bởi nhóm Windows Nâng cao \n Trần Quang Thoại \n Trịnh Thị Anh \n Đặng Minh Dương";
+        }
+
+      
+    }
+}
